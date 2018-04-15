95b799697200c31b4c7ada2f139075ea59af1b62
Cesar Saavedra <Andrs917@gmail.com>
2018-04-15 16:28:09 -0500

    Simulacion

--- trabajo1.R
+++ trabajo1.R
@@ -13,25 +13,27 @@ P <- rpois(nsim, 1.99)
 q <- table(P)
 
 
-U <- runif(nsim,0,1)
+E <- rexp(nsim,1.99)
+# Transformacion #
+Y = -(1/1.99)*(log(E))
 
-
-plot(q, freq=T)
-plot(density(q))
+plot(P)
+plot(Y)
 
 #-----------------------------------#
 #Distribucion Logistic:
 #-----------------------------------#
-L <- rlogis(nsim,7.485,0.01771)
-L2 <- rlogis(nsim,7.482,0.0256)
+L <- rlogis(nsim, 3,4)
 
 U <- runif(nsim,0,1)
 # Transformacion #
 X = log((U)/(1-U))
-l <- seq(-8,8, by=0.001)
+
 
 par(mfrow=c(1,2))
 hist((L), main="Logic Tradicional")
 hist((X), main="Logic Transformacion", freq=F)
-lines(l, dlogis(l,1,3))
 
+
+
+       
