ab27c35b69165ceadcf2365ef0d136faa6457fb5
Cesar Saavedra <Andrs917@gmail.com>
2018-04-15 10:24:51 -0500

    Simulacion

--- trabajo1.R
+++ trabajo1.R
@@ -1,4 +1,37 @@
-#Distribución Poisson:
+#-----------------------------------#
+# Kevin Steven Garcia Chica
+# Cesar A. Saavedra 
+# Simulacion Estadistica Trabajo 1
+#-----------------------------------#
+
+nsim=10000
+
+#-----------------------------------#
+#Distribucion Poisson:
+#-----------------------------------#
+P <- rpois(nsim, 1.99)
+q <- table(P)
 
 
-#Distribución Logistic:
+U <- runif(nsim,0,1)
+
+
+plot(q, freq=T)
+plot(density(q))
+
+#-----------------------------------#
+#Distribucion Logistic:
+#-----------------------------------#
+L <- rlogis(nsim,7.485,0.01771)
+L2 <- rlogis(nsim,7.482,0.0256)
+
+U <- runif(nsim,0,1)
+# Transformacion #
+X = log((U)/(1-U))
+l <- seq(-8,8, by=0.001)
+
+par(mfrow=c(1,2))
+hist((L), main="Logic Tradicional")
+hist((X), main="Logic Transformacion", freq=F)
+lines(l, dlogis(l,1,3))
+
