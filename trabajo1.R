f95e6d1e675c514b0dbcd8a8d2fc046cb9528ea9
Cesar Saavedra <Andrs917@gmail.com>
2018-04-18 22:51:29 -0500

    Algo +

--- trabajo1.R
+++ trabajo1.R
@@ -3,37 +3,56 @@
 # Cesar A. Saavedra 
 # Simulacion Estadistica Trabajo 1
 #-----------------------------------#
-
+suppressMessages(library(ggplot2))
 nsim=10000
-
 #-----------------------------------#
 #Distribucion Poisson:
 #-----------------------------------#
+# M. tradicional #
 P <- rpois(nsim, 1.99)
-q <- table(P)
 
 
-E <- rexp(nsim,1.99)
 # Transformacion #
+x=0
+t=0
+X <- rexp(nsim,1/1.99)
+t=t+X
+
+for (x in t) {
+  
+}
 Y = -(1/1.99)*(log(E))
 
-plot(P)
-plot(Y)
+#Grafico#
+barplot(table(P), xlab="Magnitud", ylab="Tasa de Terremotos", main="Probabilidad de Riesgo Sismico")
+qplot(P, main="Distribucion Poisson para Riesgo Sismico", 
+      xlab="Magnitud", ylab="Tasa de Terremotos", col=I("black"),fill=I("red")) 
+
+
+
 
 #-----------------------------------#
 #Distribucion Logistic:
 #-----------------------------------#
+# M. tradicional #
 L <- rlogis(nsim, 3,4)
 
-U <- runif(nsim,0,1)
 # Transformacion #
-X = log((U)/(1-U))
+a=3
+b=4
+U <- runif(nsim,0,1)
+X = a + b*log((U)/(1-U))
+
+datos <- data.frame(L,X)
+
+#Grafico#
+qplot(L, main="Distribucion Logistica", col=I("black"), fill=I("grey")) 
+qplot(X, main="Transformacion Distribucion Logistica", col=I("black"), fill=I("grey"))
 
 
+#Grafico#
 par(mfrow=c(1,2))
-hist((L), main="Logic Tradicional")
-hist((X), main="Logic Transformacion", freq=F)
+hist((L), main="Logic Tradicional", freq=F, xlab="", yalb="", col="magenta1", bty="n")
+hist((X), main="Logic Transformacion", freq=F, xlab="", yalb="", col="purple1", bty="n") 
 
 
-
-       
